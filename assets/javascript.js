40c48b791c553b1763dd8fb50e7fcf903ae79d16
Dylson <Dylsonuni@gmail.com>
2021-06-19 13:52:38 +0100

    Allocated job roles in css and java

---
+++ assets/javascript.js
@@ -0,0 +1,7 @@
+/* Have a look at activity 16 & 21 first bullet point*/ 
+
+/* Have a google search of how hangman works,  */
+
+/* Have a look at activities 9 & 10 for the timer 3rd and 4th bullet point */
+
+/* Have a look at activity 13 for the final bullet point.  */
